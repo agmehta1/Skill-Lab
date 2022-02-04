ae892e76fa54fc4940815da66c22fcbcfae26493
agmehta1 <agmehta@ucsd.edu>
2022-02-04 15:14:16 -0800

    junit and not working code

--- SkillLab.java
+++ SkillLab.java
@@ -1,1 +1,6 @@
 
+public class SkillLab {
+    public static int add(int n,int m) {
+        return n+m;
+    }
+}
---
+++ SkillLabTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+ 
+public class SkillLabTest {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillLab.add(1,3));
+    }
+}
